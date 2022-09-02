6c71f2d53d24c05b49bfa13d1ea3de4ba93a8dea
andradebru <brunansandrade@gmail.com>
2022-09-02 14:14:48 -0300

    deleta tarefa

--- todo.js
+++ todo.js
@@ -3,16 +3,32 @@ var app = new Vue({
   data: {
     message: "Olá Vue!",
     tasks: [],
+    modoAdicionar: false,
   },
   methods: {
     getTasks() {
       fetch("http://localhost:3000/tasks")
         .then((response) => response.json())
-        .then((tarefasJson) => (this.tasks = tarefasJson));
+        .then((tarefasJson) => {
+          console.log(tarefasJson);
+          this.tasks = tarefasJson;
+        });
+    },
+    adicionar() {
+      this.modoAdicionar = true;
+    },
+    salvar() {
+      this.modoAdicionar = false;
+    },
+    deletar(id) {
+      fetch(`http://localhost:3000/tasks/${id}`, { method: "DELETE" });
     },
   },
   created() {
-    console.log("funcionou created");
+    console.log("created");
     this.getTasks();
+  },
+  mounted() {
+    console.log("montend");
   },
 });
